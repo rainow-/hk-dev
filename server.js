148133974adaaa46e8a6604fc21f6ceff327f0e7
Thomas <thomas@ftb.darknoddy.com>
2017-08-27 14:03:35 +0200

    Changes to server side routing

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require('path');
 const app = express();
 
 const DATA_FILE = path.join(__dirname, 'data.json');
+const STATIC_DIR = path.join(__dirname, 'client/build/');
+
+console.log("STATIC DIR:", STATIC_DIR);
+
+app.use(express.static(STATIC_DIR));
 
 app.get('/api/gallery', (req, res) => {
 	const perPage = req.query.per_page;
@@ -30,6 +35,10 @@ app.get('/api/gallery', (req, res) => {
 		res.setHeader('Cache-Control', 'no-cache');
 		res.json({photoset: { photo: photoSet }, pages: pages});
 	});
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(STATIC_DIR + 'index.html');
 }); 
 
 module.exports = app;
